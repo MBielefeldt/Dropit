0f391544a7e8f3c29cf06606d3454ac791fcd6dd
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2014-05-18 23:34:59 +0200

    Animation options added

--- Dropit/DropitBehaviour.m
+++ Dropit/DropitBehaviour.m
@@ -11,6 +11,7 @@
 @interface DropitBehaviour ()
 @property (strong, nonatomic) UIGravityBehavior *gravity;
 @property (strong, nonatomic) UICollisionBehavior *collider;
+@property (strong, nonatomic) UIDynamicItemBehavior *animationOptions;
 @end
 
 @implementation DropitBehaviour
@@ -23,6 +24,7 @@
     {
         [self addChildBehavior:self.gravity];
         [self addChildBehavior:self.collider];
+        [self addChildBehavior:self.animationOptions];
     }
     
     return self;
@@ -33,7 +35,7 @@
     if (!_gravity)
     {
         _gravity = [[UIGravityBehavior alloc] init];
-        _gravity.magnitude = 0.9;
+        _gravity.magnitude = 1.0;
     }
     return _gravity;
 }
@@ -49,16 +51,28 @@
     return _collider;
 }
 
+- (UIDynamicItemBehavior *)animationOptions
+{
+    if (!_animationOptions)
+    {
+        _animationOptions = [[UIDynamicItemBehavior alloc] init];
+        _animationOptions.allowsRotation = NO;
+    }
+    return _animationOptions;
+}
+
 - (void)addItem:(id <UIDynamicItem>)item
 {
     [self.gravity addItem:item];
     [self.collider addItem:item];
+    [self.animationOptions addItem:item];
 }
 
 - (void)removeItem:(id <UIDynamicItem>)item
 {
     [self.gravity removeItem:item];
     [self.collider removeItem:item];
+    [self.animationOptions removeItem:item];
 }
 
 @end
